f6387c40e42e7166a941c3eaff72f3d97ae5bb36
TesDevelopment <joshuaepee1@gmail.com>
2024-05-05 20:17:25 -0400

    Start working on HAL

--- modules/hal.go
+++ modules/hal.go
@@ -1,1 +1,33 @@
 package modules
+
+// Once again credit to qhdwight for his basis
+
+// #cgo CFLAGS: -I${SRCDIR}/include
+// #cgo LDFLAGS: -L${SRCDIR}/../../build -lwpiHal -lwpiutil -lstdc++ -ldl -lm -lFRC_NetworkCommunication -lembcanshim -lfpgalvshim -lRoboRIO_FRC_ChipObject -lvisa
+// #include "hal.h"
+import "C"
+import (
+	"os"
+	"unsafe"
+)
+
+const (
+	FEnabled = 1 << iota
+	FAutonomous
+	FTest
+	FEStop
+	FFMSAttached
+	FDSAttached
+)
+
+const (
+	None = iota
+	Disabled
+	Autonomous
+	Teleop
+	Test
+)
+
+func Loader(L *lua.LState) int {
+	
+}
